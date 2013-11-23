84fb1419a62b6ffa8836dd42408b069696496624
tekniklr <tekniklr@tekniklr.com>
2013-11-23 14:45:40 -0800

    Path to postgres :/

--- custom/plugins/tekniklr/tekniklr.plugin.zsh
+++ custom/plugins/tekniklr/tekniklr.plugin.zsh
@@ -26,7 +26,7 @@ compdef _program program
 
 # environment
 export PERL5LIB=/Library/Perl/5.16:/System/Library/Perl/Extras/5.16/darwin-thread-multi-2level:/System/Library/Perl/Extras/5.16
-PATH=$PATH:$HOME/bin:$HOME/Dropbox/programs/bin:$HOME/Dropbox/bin:$HOME/.rbenv/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
+PATH=$PATH:$HOME/bin:$HOME/Dropbox/programs/bin:$HOME/Dropbox/bin:$HOME/.rbenv/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/opt/local/lib/postgresql84/bin
 
 
 # set up ruby
