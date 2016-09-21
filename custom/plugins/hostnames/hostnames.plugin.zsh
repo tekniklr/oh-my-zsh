6c7f1dbcd3ae40d543b29bf895566a90007dc67b
tekniklr <tsolow@tekniklr.com>
2016-09-21 16:37:20 -0700

    clarify what this does

--- custom/plugins/hostnames/hostnames.plugin.zsh
+++ custom/plugins/hostnames/hostnames.plugin.zsh
@@ -1,6 +1,7 @@
 # copied from the profiles plugin, this looks only at the first part of the 
 # hostname, instead of the last part of the hostname
-# (e.g., given 'host.example.com', matches 'host' instead of 'example.com')
+# (e.g., given 'host.example.com', matches 'host' and 'host.example' instead 
+# of 'example.com' and 'com')
 
 # You will probably want to list this plugin as the first in your .zshrc.
 
