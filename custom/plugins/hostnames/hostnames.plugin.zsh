1c6c99937d14f46284037ed14e391e80b9749691
tekniklr <tsolow@tekniklr.com>
2016-10-05 13:41:51 -0700

    add note on how to unset manually defined hostnames

--- custom/plugins/hostnames/hostnames.plugin.zsh
+++ custom/plugins/hostnames/hostnames.plugin.zsh
@@ -18,3 +18,7 @@ done
 # note: on macOS some wi-fi networks will abscond with your hostname! force 
 # the correct hostname with `sudo scutil --set HostName <my cool hostname>`
 # https://apple.stackexchange.com/questions/50022/hostname-changes-in-the-terminal-when-connecting-to-some-wifi-networks
+
+# note re: note: you can unset hostnames defined this way (in case a service 
+# relies on the dhcp hostname) with: `sudo scutil --set HostName ''`
+# https://jamfnation.jamfsoftware.com/discussion.html?id=15411
