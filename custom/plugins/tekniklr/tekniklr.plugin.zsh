2e374ffd1faf245bab9cd2e2f2bbd8d939a3356e
tekniklr <tsolow@tekniklr.com>
2017-06-21 18:21:55 -0700

    tildas aren't that nice

--- custom/plugins/tekniklr/tekniklr.plugin.zsh
+++ custom/plugins/tekniklr/tekniklr.plugin.zsh
@@ -38,7 +38,7 @@ compdef _program program
 # environment
 export EDITOR=~/bin/subl
 export PERL5LIB=/Library/Perl/5.16:/System/Library/Perl/Extras/5.16/darwin-thread-multi-2level:/System/Library/Perl/Extras/5.16
-export PATH=~$PATH:~/bin:/bin:~/Dropbox/programs/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+export PATH=$PATH:~/bin:/bin:~/Dropbox/programs/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 
 # rbenv
 eval "$(rbenv init -)"
